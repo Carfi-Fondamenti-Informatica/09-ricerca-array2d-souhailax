c7541e7465dad73d7fd82224fb2a511cd90abf4d
souhailax <117657684+souhailax@users.noreply.github.com>
2022-12-17 22:10:20 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+bool cerca(char lista[10][20], int &posizione, char target[]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if(lista[i][j] != target[j]){
+                j = 20;
+            }else{
+              if(j==19){
+                  posizione = i;
+                  return true;
+              }
+            }
+        }
+    }
+    return false;
+}
